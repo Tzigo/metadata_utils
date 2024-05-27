1f1e9b02a091c590536eaf9fea2a15a10e2172c5
Tzigo <drizzt9@gmx.de>
2024-05-27 21:51:44 +0200

    Update metadata_adder.py
    Minor bugfixes

--- metadata_adder/scripts/metadata_adder.py
+++ metadata_adder/scripts/metadata_adder.py
@@ -9,6 +9,7 @@ import json
 
 from modules import script_callbacks
 
+
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
         with gr.Row():
@@ -25,7 +26,7 @@ def on_ui_tabs():
             json_input = gr.Textbox(placeholder='Input JSON content', max_lines=10, elem_id="json_input",
                                     label="Metadata as JSON (SHIFT + SPACE = new line)")
 
-        with gr.Row() as row:
+        with gr.Row():
             gr.Column(scale=1)
             with gr.Column(scale=1):
                 button = gr.Button(value="Add Metadata", variant="primary")
@@ -39,23 +40,29 @@ script_callbacks.on_ui_tabs(on_ui_tabs)
 
 
 def on_button(input_file: str, new_name: str, json_input: str):
+    is_forge = False
+    main_path = os.getcwd()
+    automatic1111_ckpt_folder = None
 
-    Isforge = False
-    getforge = "Forge"
-    mainpath = os.getcwd()
-    if getforge in mainpath:
-            Isforge = True
-            webUIbatpath = mainpath + "\\" + "webui-user.bat"
+    # Forge support
+    # get checkpoint directory from user config batch file
+    if 'Forge' in main_path:
+        is_forge = True
+        web_ui_batpath = main_path + '\\' + 'webui-user.bat'
 
-            with open(webUIbatpath, 'r', encoding='utf-8') as file:
-                text = file.read()        
-            starts_with = '--ckpt-dir "'
-            ends_with = '"'
-    
-            start_index = text.find(starts_with) + len(starts_with)
-            end_index = text.find(ends_with, start_index)
-            if start_index != -1 and end_index != -1:
-                automatic1111ckptfolder = text[start_index:end_index]  
+        with open(web_ui_batpath, 'r', encoding='utf-8') as file:
+            text = file.read()
+        starts_with = '--ckpt-dir "'
+        ends_with = '"'
+
+        # extract directory from flag
+        start_index = text.find(starts_with) + len(starts_with)
+        end_index = text.find(ends_with, start_index)
+        if start_index != -1 and end_index != -1:
+            automatic1111_ckpt_folder = text[start_index:end_index]
+        else:
+            gr.Warning('Please check your Forge ckpt-dir flag in "webui-user.bat"!')
+            return
 
     # Stop if no checkpoint is selected
     if len(input_file) == 0:
@@ -89,45 +96,43 @@ def on_button(input_file: str, new_name: str, json_input: str):
             new_name = new_name + ".safetensors"
 
     # Build new path from global model path and new file name
-    if Isforge == False:
+    if not is_forge:
         new_path = models.model_path + "\\" + new_name
-    else:     
-        forge_filename = input_file.split(".safetensors")[0] + ".safetensors"       
-        filehavefound = False
-        
-        for dirpath, dirnames, filenames in os.walk(automatic1111ckptfolder):
-            if forge_filename in filenames:
-                forge_filepath = os.path.join(dirpath, forge_filename)
-                filehavefound = True
+    else:
+        new_path = automatic1111_ckpt_folder + "\\" + new_name
+
+        # get path of editing checkpoint
+        forge_filename = input_file.split(".safetensors")[0] + ".safetensors"
+        file_found = False
+
+        # find editing checkpoint in files
+        for dir_path, _, file_names in os.walk(automatic1111_ckpt_folder):
+            if forge_filename in file_names:
+                # set current path
+                file_path = os.path.join(dir_path, forge_filename)
+                file_found = True
                 break
-                
-            else:
-               filehavefound = False
-               return
-                
-        if filehavefound != True:
+
+        if not file_found:
             gr.Warning("file not found")
-        else:
-            gr.Info("Saving Checkpoint...")
-            print("\nSaving Checkpoint...")
+            return
+
+    # check if file already exists
+    if os.path.isfile(new_path):
+        gr.Warning("Name already existing!")
+        return
+
+    log("Saving Checkpoint...")
 
     # extract tensors from selected checkpoint
-            if Isforge:
-                loaded_tensors = load_file(forge_filepath) 
-            else:   
-                loaded_tensors = load_file(file_path)
+    loaded_tensors = load_file(file_path)
 
     # save extracted checkpoints into new file, along with metadata
-            if Isforge:
-                save_file(loaded_tensors, automatic1111ckptfolder + "\\" + new_name, mdata)
-            else:
-                save_file(loaded_tensors, new_path, mdata)
+    save_file(loaded_tensors, new_path, mdata)
 
-            gr.Info("Saved Checkpoint!")
-            print("\nSaved Checkpoint!")
-
-        
+    log("Saved Checkpoint!")
 
 
-
-    
+def log(message):
+    gr.Info(message)
+    print("\n"+message)
