ea0964d5c46577bc690c64e366f76ac428f6ee01
FelixFelix0815 <64499644+FelixFelix0815@users.noreply.github.com>
2024-09-27 17:47:27 +0200

    merged reader and adder into one window and added lora support

---
+++ metadata_utils_file_utils.py
@@ -0,0 +1,56 @@
+import modules.sd_models as models
+import os
+
+lora_list = []
+lora_path = os.path.join(models.paths.models_path, "Lora")
+
+
+def list_loras():
+    global lora_list
+    if not os.path.isdir(lora_path):
+        print("failed")
+        return
+
+    def list_recursive(path: str) -> list[str]:
+        out = []
+        global_path = os.path.join(lora_path, path)
+        for item in os.listdir(global_path):
+            if os.path.isfile(os.path.join(global_path, item)):
+                out.append(os.path.join(path, item))
+            elif os.path.isdir(os.path.join(global_path, item)):
+                out.extend(list_recursive(os.path.join(path, item)))
+
+        return out
+
+    lora_list = list_recursive("")
+
+
+def get_lora(lora):
+    if not os.path.isfile(os.path.join(lora_path, lora)):
+        return None
+    return os.path.join(lora_path, lora)
+
+
+class LoraFileImitat:
+    def __init__(self, lora):
+        self.is_safetensors = lora.endswith(".safetensors")
+        self.filename = os.path.join(lora_path, lora)
+
+
+def get_lora_on_button(lora):
+    print(lora_path)
+    print(lora)
+    print(os.path.isfile(os.path.join(lora_path, lora)))
+    if not os.path.isfile(os.path.join(lora_path, lora)):
+        return None
+    return LoraFileImitat(lora)
+
+
+def lora_tiles():
+    global lora_list
+    if len(lora_list) == 0:
+        list_loras()
+    return lora_list
+
+
+list_loras()
--- scripts/metadata_utils.py
+++ scripts/metadata_utils.py
@@ -2,6 +2,7 @@ import gradio as gr
 import modules.sd_models as models
 from modules.ui import create_refresh_button
 from metadata_util_lib import write_metadata
+import metadata_utils_file_utils as file_utils
 from pathlib import Path
 import json
 
@@ -10,79 +11,58 @@ from modules import script_callbacks
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
-        
-        with gr.Tab("Reader"):
-            with gr.Tab("Checkpoint"):
-                        with gr.Row():
-                            input_file = gr.Dropdown(models.checkpoint_tiles(), label="Checkpoint")
-                            create_refresh_button(input_file, models.list_models,
-                                      lambda: {"choices": models.checkpoint_tiles()}, "metadata_utils_refresh_2")
-                        with gr.Row():
-                            metadata = gr.Code(label="Metadata", language="json")
-
-                            input_file.change(
-                            fn=on_button_load_metadata,
-                            inputs=[input_file],
-                            outputs=[metadata]
-                        )
-            with gr.Tab("Lora"):
-                            with gr.Row():
-                                input_file = gr.Dropdown(models.checkpoint_tiles(), label="Lora")
-                                create_refresh_button(input_file, models.list_models,
-                                      lambda: {"choices": models.checkpoint_tiles()}, "metadata_utils_refresh_2")
-                            with gr.Row():
-                                metadata = gr.Code(label="Metadata", language="json")
-
-                                input_file.change(
-                                fn=on_button_load_metadata,
-                                inputs=[input_file],
-                                outputs=[metadata]
-            )
-            
-        with gr.Tab("Adder"):
-            with gr.Tab("Checkpoint"):
-                            with gr.Row():
-                                input_file = gr.Dropdown(models.checkpoint_tiles(), label="Checkpoint")
-                                create_refresh_button(input_file, models.list_models,
+        with gr.Tab("Checkpoint"):
+            with gr.Row():
+                input_file = gr.Dropdown(models.checkpoint_tiles(), label="Checkpoint")
+                create_refresh_button(input_file, models.list_models,
                                       lambda: {"choices": models.checkpoint_tiles()}, "metadata_utils_refresh_1")
 
-                                button = gr.Button(value="Add Metadata", variant="primary")
+                button = gr.Button(value="Set Metadata", variant="primary")
 
-                            gr.HTML("<p style=\"text-align:center;color:red\">Warning! Changing the metadata of "
-                                    "your checkpoint also changes it's hash</p>")
+            gr.HTML("<p style=\"text-align:center;color:red\">Warning! Changing the metadata of "
+                    "your checkpoint also changes it's hash</p>")
 
-                            with gr.Row():
-                                    new_name = gr.Textbox(
-                                    placeholder='(Optional) Enter new checkpoint name. If omitted, appends "_md" to name',
-                                    max_lines=1, label="New Name")
+            with gr.Row():
+                new_name = gr.Textbox(
+                    placeholder='(Optional) Enter new checkpoint name. If omitted, appends "_md" to name',
+                    max_lines=1, label="New Name")
 
-                            with gr.Row():
-                                    json_input = gr.Code(lines=10,
-                                                        label="Metadata as JSON", language="json")
+            with gr.Row():
+                json_input = gr.Code(lines=10,
+                                     label="Metadata as JSON", language="json")
+                input_file.change(
+                    fn=on_button_load_metadata,
+                    inputs=[input_file],
+                    outputs=[json_input]
+                )
 
-                            button.click(on_button, inputs=[input_file, new_name, json_input])
-            with gr.Tab("Lora"):
-                        with gr.Row():
-                                input_file = gr.Dropdown(models.checkpoint_tiles(), label="Lora")
-                                create_refresh_button(input_file, models.list_models,
-                                      lambda: {"choices": models.checkpoint_tiles()}, "metadata_utils_refresh_1")
+            button.click(on_button, inputs=[input_file, new_name, json_input])
+        with gr.Tab("Lora"):
+            with gr.Row():
+                input_file = gr.Dropdown(file_utils.lora_tiles(), label="Lora")
+                create_refresh_button(input_file, file_utils.list_loras,
+                                      lambda: {"choices": file_utils.lora_tiles()}, "metadata_utils_refresh_1")
 
-                                button = gr.Button(value="Add Metadata", variant="primary")
+                button = gr.Button(value="Set Metadata", variant="primary")
 
-                        gr.HTML("<p style=\"text-align:center;color:red\">Warning! Changing the metadata of "
-                                    "your model also changes it's hash</p>")
+            gr.HTML("<p style=\"text-align:center;color:red\">Warning! Changing the metadata of "
+                    "your model also changes it's hash</p>")
 
-                        with gr.Row():
-                                    new_name = gr.Textbox(
-                                    placeholder='(Optional) Enter new model name. If omitted, appends "_md" to name',
-                                    max_lines=1, label="New Name")
+            with gr.Row():
+                new_name = gr.Textbox(
+                    placeholder='(Optional) Enter new model name. If omitted, appends "_md" to name',
+                    max_lines=1, label="New Name")
 
-                        with gr.Row():
-                                        json_input = gr.Code(lines=10,
-                                        label="Metadata as JSON", language="json")
+            with gr.Row():
+                json_input = gr.Code(lines=10,
+                                     label="Metadata as JSON", language="json")
+                input_file.change(
+                    fn=on_button_load_metadata_lora,
+                    inputs=[input_file],
+                    outputs=[json_input]
+                )
 
-                        button.click(on_button, inputs=[input_file, new_name, json_input])                    
-
+            button.click(on_button_lora_wrapper, inputs=[input_file, new_name, json_input])
 
     return [(ui_component, "Metadata Utils", "metadata_utils_tab")]
 
@@ -98,8 +78,22 @@ def on_button_load_metadata(input_file: str):
     return 'Model not found'
 
 
-def on_button(input_file: str, new_name: str, json_input: str):
-    selected_model = models.get_closet_checkpoint_match(input_file)
+def on_button_load_metadata_lora(input_file: str):
+    if selected_model := file_utils.get_lora(input_file):
+        if metadata := models.read_metadata_from_safetensors(selected_model):
+            return json.dumps(metadata, indent=4, ensure_ascii=False)
+        return 'No metadata'
+    return 'Model not found'
+
+
+def on_button_lora_wrapper(input_file: str, new_name: str, json_input: str):
+    on_button(input_file, new_name, json_input, True)
+
+
+def on_button(input_file: str, new_name: str, json_input: str, lora: bool = False):
+    selected_model = file_utils.get_lora_on_button(input_file) if lora \
+        else models.get_closet_checkpoint_match(input_file)
+
     if not selected_model:
         gr.Warning("Please select a model")
         return
